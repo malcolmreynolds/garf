96111fd942c2fbc5098230b3cff774e8346eb163
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2013-05-01 15:39:33 +0100

    deleted old crap

--- old_crap/src/training_set/supervised_training_set_impl.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef GARF_SUPERVISED_TRAINING_SET_IMPL_HPP
-#define GARF_SUPERVISED_TRAINING_SET_IMPL_HPP
-
-template<typename FeatT, typename LabelT>
-supervised_training_set<FeatT, LabelT>::supervised_training_set(boost::shared_ptr<typename garf_types<FeatT>::matrix> feature_matrix) 
-    : training_set<FeatT>(feature_matrix) { 
-}
-
-#endif
